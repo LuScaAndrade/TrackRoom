e9850e5c47d0e2cfaef27aab25b00d85f1d356f6
LuScaAndrade <Lucasavitorino1@gmail.com>
2025-06-17 14:43:24 -0300

    feat: adding home page

--- FrontEnd/trackRoom/src/App.tsx
+++ FrontEnd/trackRoom/src/App.tsx
@@ -1,9 +1,10 @@
 import { Routes, Route } from "react-router-dom"
+import Home from "./pages/Home.tsx"
 
 function App() {
   return (
     <Routes>
-      <Route index></Route>
+      <Route path="/" element={<Home />}></Route>
     </Routes>
   )
 }
---
+++ FrontEnd/trackRoom/src/pages/Home.tsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <h1 className="bg-black text-white">Home</h1>
+}
+
+export default Home
